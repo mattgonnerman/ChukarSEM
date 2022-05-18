325f16aec1e2da5e61be2b5681e4c85b1f72e6b2
Matt Gonnerman <matthew.gonnerman@maine.edu>
2022-05-18 12:48:36 -0400

    Shiny app practice code backup

---
+++ app.R
@@ -0,0 +1,163 @@
+#https://shiny.rstudio.com/images/shiny-cheatsheet.pdf
+#https://vimeo.com/131218530?embedded=true&source=video_title&owner=22717988
+#https://ecoforecast.org/workshops/r-shiny-seminar-series/
+
+
+
+
+
+
+# Load packages
+lapply(c("shiny"), require, character.only = T)
+
+##################################################################################
+### Setup page with a navigation bar
+ui <- navbarPage( title = "NDOW Upland Harvest",
+  
+  tabPanel(
+    title = "Instructions",
+    
+    p("This is where instructions will go!")),
+  
+  tabPanel(
+    title = "Harvest Forecast"
+    
+    ),
+  
+  tabPanel(
+    title = "Species-Specific"
+    
+    ),
+  
+  tabPanel(
+    title = "Download/Upload Data",
+    
+    p("This is where you will be able to view the currently available data,
+      download data that you need, and input new data as it becomes available."),
+           
+    #Download Specific CSV to input/download data
+    selectInput("dataset", "Choose a dataset:",
+                choices = c("Hunter Effort", "Total Harvest", "Chukar Site Abundance", "Sage Grouse Wing-Bee")),
+    
+    downloadButton("downloadData", "Download Current Dataset")   
+    
+           )
+)
+
+
+##################################################################################
+### Server Instructions
+server <-  function(input,output){
+  
+  
+  ### "Download/Input Data"
+  # Reactive value for selected dataset ----
+  downdataDF <- reactive({
+    
+    switch(input$dataset,
+           "Hunter Effort" = cars,
+           "Total Harvest" = read.csv("./www/harv.csv"),
+           "Chukar Site Abundance" = cars,
+           "Sage Grouse Wing-Bee" = cars
+           )
+  })
+  
+  # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(downdataDF(), file, row.names = F)
+    }
+  )
+}
+
+
+##################################################################################
+### Construct Shiny App
+shinyApp(ui = ui, server = server)
+
+
+
+
+# ################################################################################
+# ### Example code from the RStudio Tutorial Video
+# 
+# 
+# ## CSS Options
+# ## Can stylize page using premade or adjusted css files which can be found online or made
+# 
+# 
+# ### User Interface
+# #can alternatively use fixedPage(), dashboardPage(), or navbarPage()/navbarMenu()
+# ui <- fluidPage(
+#   sidebarLayout(
+#     sidebarPanel(),
+#     mainPanel()
+#   ),
+#   
+#   fluidRow(column(3), #number is width, out of 12 units
+#            column(5)),
+#   fluidRow(4, offset = 8), #same for offset
+#   
+#   wellPanel(), #visually combines objects together in output
+#   #Many types of panels
+#   tabPanel("Tab 1", ...), #Acts as its own ui, needs a title, then list any elements using...
+#   tabsetPanel(
+#     tabPanel("tab 1", "contents"),
+#     tabPanel("tab 2", "contents"),
+#     tabPanel("tab 3", "contents")
+#   ), #combines panels together
+#   
+#   navlistPanel(), #alternative config for tabs using a sidebar
+#   
+#   fluidh1("Upland Harvest Trends"),
+#   p("Here are the", tags$strong("Instructions")),
+#   
+#   hr(), #line across to seperate sections
+#   
+#   img(height = 1000,
+#       width = 1000,
+#       src = "https://www.URL.com/r.jpg"), #can also link to file just by name.ext in app directory folder when placed in "www" folder
+#   
+#   #Input() functions
+#   sliderInput(inputId = "num", 
+#               label = "Choose a number",
+#               value = 25,
+#               min = 1,
+#               max = 100),
+#   
+#   #Output() fuynctions
+#   plotOutput(outoutId = "hist"),
+#   
+#   actionButton(inputID = "clicks",
+#                label = "Click Me")
+# )
+# 
+# ### Server Instructions
+# server <-  function(input,output){
+#   
+#   data <- reactive({
+#     rnorm(input$num)
+#   })
+#   
+#   output$hist <- renderPlot({
+#     hist(rnorm(data()))
+#   })
+#   
+#   output$stats <- renderPrint({
+#     summary(data())
+#   })
+#   
+#   observeEvent(input$go, {
+#     print(input$clicks)
+#   })
+#   
+#   reactiveEvent()
+#   
+#   reactiveValues(data = rnorm(100))
+# }
+# 
+# ### Construct Shiny App
+# shinyApp(ui = ui, server = server)
